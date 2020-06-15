ae9e1c77981d98a0bc6e67c0ad01005b2d5054d1
taafss <taafss@outlook.com>
2020-06-15 20:23:26 -0300

    Correção de cards de vídeo sobrepostos

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # mostra-pibex-2020
-Repositório para apresentação dos resultados do programa PIBEX/IFPE 2020
-
-https://taafss.github.io/mostra-pibex-2020/
+Repositório para apresentação dos resultados do programa PIBEX/IFPE 2019
---
+++ js/script.js
@@ -0,0 +1,41 @@
+(function () {
+
+  'use strict';
+
+  var $projects = $('.projects');
+
+  $projects.isotope({
+    itemSelector: '.item',
+    layoutMode: 'fitRows'
+  });
+
+  $projects.imagesLoaded().progress( function() {
+    $projects.isotope('layout');
+  });
+
+  $('ul.filters > li').on('click', function (e) {
+
+    e.preventDefault();
+
+    var filter = $(this).attr('data-filter');
+
+    $('ul.filters > li').removeClass('active');
+    $(this).addClass('active');
+
+    $projects.isotope({ filter: filter });
+
+  });
+
+  $('.card').mouseenter(function () {
+
+    $(this).find('.card-overlay').css({ 'top': '-100%' });
+    $(this).find('.card-hover').css({ 'top': '0' });
+
+  }).mouseleave(function () {
+
+    $(this).find('.card-overlay').css({ 'top': '0' });
+    $(this).find('.card-hover').css({ 'top': '100%' });
+
+  });
+
+})(jQuery);
